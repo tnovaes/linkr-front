15ef674bc67ad8286b311ad1c5e8d5c04681983a
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-30 11:19:57 -0300

    chore: add resetStyles/globalStyles and browserRoute into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle.js";
+import ResetStyle from "./styles/ResetStyle.js";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ResetStyle />
+    <GlobalStyle />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
