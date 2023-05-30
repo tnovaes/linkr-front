e230789a332ecc85d157c6ed798834c9c94b3a64
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-30 11:17:14 -0300

    refactor: little changes in globalStyle

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,10 +2,6 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     * {
-        font-family: 'Lato';
-        font-weight: 400;
-        font-size: 17px;
-        line-height: 20px;
         box-sizing:border-box;
     }
     span {
@@ -15,23 +11,20 @@ const GlobalStyle = createGlobalStyle`
         height:100%;
         width:100%;
     }
+    #root div {
+        width:100%;
+    }
     svg {
         cursor: pointer;
     }
     button, input {
         outline: none;
         border: none;
-        border-radius: 6px;;
-        cursor: pointer;
+        border-radius: 6px;
     }
     input{
         background-color:white;
         color:#9f9f9f;
-        font-family: 'Oswald';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 27px;
-        line-height: 40px;
     }
     button {
         font-family: 'Lato';
@@ -40,11 +33,15 @@ const GlobalStyle = createGlobalStyle`
         line-height: 17px;
         background-color: #1877f2;
         color:white;
+        cursor:pointer;
     }
     h1 {
         font-weight: 700;
         font-size: 26px;
         color: white;
+    }
+    a {
+        cursor:pointer;
     }
 `
 
