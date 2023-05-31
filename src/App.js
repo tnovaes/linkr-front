bc38df04e286e963e543fcb4faaf792bd37990b0
TiagoNovaes <tiagousnovaes@gmail.com>
2023-05-31 18:36:27 -0300

    feat: timeline page static layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import SignInPage from "./pages/SignInPage.js";
 import SignUpPage from "./pages/SignUpPage.js";
 import { Header } from "./components/Header.js";
+import TimelinePage from "./pages/TimelinePage.js";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/timeline" element={<Header><TimelinePage /></Header>} />
         {/* demo de como usar o header, só por em volta do componente da página, podem apagar depois */}
         <Route path="/demoheader" element={<Header><SignUpPage /></Header>} />
       </Routes>
