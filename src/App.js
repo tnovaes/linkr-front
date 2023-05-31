a5f66ca13cdaa5b69d8f5795f53942f384fc9366
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-31 10:53:51 -0300

    chore: Demo how to use Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import SignInPage from "./pages/SignInPage.js";
 import SignUpPage from "./pages/SignUpPage.js";
+import { Header } from "./components/Header.js";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        {/* demo de como usar o header, só por em volta do componente da página, podem apagar depois */}
+        <Route path="/demoheader" element={<Header><SignUpPage /></Header>} />
       </Routes>
     </>
   );
