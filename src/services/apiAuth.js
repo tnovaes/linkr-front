7608cab015554e0afe80f69126d0b851610fa9eb
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-06-07 11:33:13 -0300

    feat: add button/option for following/unfollowing

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -9,15 +9,24 @@ function signIn(body) {
     return axios.post(`${BASE_URL}/signin`, body);
 }
 function logout(token) {
-    return axios.post(`${BASE_URL}/logout`,{}, { headers: { Authorization: token } });
+    return axios.post(`${BASE_URL}/logout`, {}, { headers: { Authorization: token } });
 }
 function getUsers(token, searchText) {
     return axios.post(`${BASE_URL}/users`, { searchText }, { headers: { Authorization: token } });
 }
-function getUserPhoto(token) {
+function getUserProfilePhoto(token) {
     return axios.get(`${BASE_URL}/users/profilePhotoUrl`, { headers: { Authorization: token } });
 }
+function getUserPhoto(token, id) {
+    return axios.get(`${BASE_URL}/users/${id}/profilePhotoUrl`, { headers: { Authorization: token } });
+}
+function setFollower(token, id, isFollowed) {
+    return axios.post(`${BASE_URL}/users/${id}/follow`, { isFollowed }, { headers: { Authorization: token } });
+}
+function getIsFollowing(token, id) {
+    return axios.get(`${BASE_URL}/users/${id}/following`, { headers: { Authorization: token } });
+}
 
-const apiAuth = { signUp, signIn, logout, getUsers, getUserPhoto };
+const apiAuth = { signUp, signIn, logout, getUsers, getUserPhoto, getUserProfilePhoto, setFollower, getIsFollowing };
 
 export default apiAuth;
