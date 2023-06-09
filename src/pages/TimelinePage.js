d6e52df121143640d12e3071ae834f308e981d0b
TiagoNovaes <tiagousnovaes@gmail.com>
2023-06-09 14:06:57 -0300

    feat: timeline with reposts

--- src/pages/TimelinePage.js
+++ src/pages/TimelinePage.js
@@ -6,14 +6,15 @@ import React, { useEffect, useRef, useState } from "react";
 import { usePhoto } from "../hooks/useImage.js";
 import trashCan from "../assets/trash.svg";
 import heart from "../assets/heart.png";
-import papperPlane from "../assets/papperPlane.svg"
+import papperPlane from "../assets/papperPlane.svg";
 import filledHeart from "../assets/filled-heart.png";
-import dialogBox from "../assets/dialogBox.svg"
+import dialogBox from "../assets/dialogBox.svg";
 import pencil from "../assets/pencil.svg";
 import repostButton from "../assets/repostButton.svg"
 import Modal from "../components/Modal.js";
 import reactStringReplace from 'react-string-replace';
 import useInterval from "use-interval";
+import RepostModal from "../components/RepostModal.js";
 
 export default function TimelinePage() {
     const [feed, setFeed] = useState();
@@ -24,6 +25,7 @@ export default function TimelinePage() {
     const [userToken, setUserToken] = useState("");
     const [reload, setReload] = useState(false);
     const [openModal, setOpenModal] = useState(false);
+    const [openRepostModal, setOpenRepostModal] = useState(false);
     const [selectedPost, setSelectedPost] = useState(0);
     const [postIndex, setpostIndex] = useState(0);
     const [isEditing, setIsEditing] = useState(false);
@@ -53,7 +55,7 @@ export default function TimelinePage() {
         setIsEditing(!isEditing);
     };
 
-    const toggleComment = (Index,id)=>{
+    const toggleComment = (Index, id) => {
         setpostIndex(Index);
         setSelectedPost(id);
         setOpenComment(!openComment);
@@ -237,6 +239,11 @@ export default function TimelinePage() {
         setOpenModal(true)
     }
 
+    function handleRepostModal(postId) {
+        setSelectedPost(postId)
+        setOpenRepostModal(true)
+    }
+
     async function handleLike(post_id) {
         try {
             const token = localStorage.getItem('token')
@@ -258,17 +265,6 @@ export default function TimelinePage() {
         window.location.reload(true);
     }
 
-    function handleRepost(post_id) {
-        const token = localStorage.getItem("token");
-        apiPosts.sharePost(token, post_id)
-        .then(res => {
-            setReload(!reload);
-        })
-        .catch(err => {
-            alert("There was an error sharing this post")
-            console.log(err.response.data);
-        });
-    }
 
     return (
         <TimelinePageContainer>
@@ -310,16 +306,72 @@ export default function TimelinePage() {
                 {isLoadingPage ? <NoFeed data-test="message" >Loading...</NoFeed> :
                     !hasFriends ? "You don't follow anyone yet. Search for new friends!" : feed.length === 0 ? "No posts found from your friends" :
                         feed.map((f, index) => {
-                            //console.log(f.comments)
-                            const cm = f.comments.map((c) => (
-                                <Comments>
-                                    <ProfileImage userProfileImage={c.writer_avatar} width="50px" height="50px" />
-                                    <ComName>
-                                        <p>Nome do cara {c.is_following && <span>• following</span>}</p>
-                                        <h1>{c.text}</h1>
-                                    </ComName>
-                                </Comments>
-                            ))
+                            const cm = f.comments.map((c) => {
+                                return (
+                                    <Comments key={c.id}>
+                                        <ProfileImage userProfileImage={c.writer_avatar} width="50px" height="50px" />
+                                        <ComName>
+                                            <p>{c.writer_name} {(Number(c.writer_id) === Number(c.post_owner)) ? <span>• post’s author</span> : c.is_following && <span>• following</span>}</p>
+                                            <h1>{c.text}</h1>
+                                        </ComName>
+                                    </Comments>
+                                )
+                            })
+                            if (f.reposter_name) {
+                                return (
+                                    <BigContainer key={f.post_id} data-test="post">
+                                        <RepostBanner>
+                                            <img src={repostButton} alt="Reposted by"></img>
+                                            <Link to={`/user/${f.post_owner}`}>
+                                                <p>Re-posted by <span>{(f.user_id === localStorage.getItem("id")) ? "you" : f.reposter_name}</span></p>
+                                            </Link>
+                                        </RepostBanner>
+                                        <PostContainer>
+                                            <SideContainer>
+                                                <ImageLikeContainer>
+                                                    <ProfileImage userProfileImage={f.avatar} width="50px" height="50px" />
+                                                    <img src={f.isLiked ? filledHeart : heart} alt="heart" />
+                                                    <p onMouseEnter={() => handleLikeHover(f.original_post_id)} onMouseOut={() => handleLikeHoverLeaving(f.original_post_id)} data-test="counter" >{f.likes} Likes</p>
+                                                    {likesInfo && f.likesInfo?.length > 0 && (<div data-test="tooltip">
+                                                        <div></div>
+                                                        <p>{f.likesInfo}</p>
+                                                    </div>)}
+                                                </ImageLikeContainer>
+                                                <DialogBox onClick={() => toggleComment(index, f.post_id)}>
+                                                    <img src={dialogBox} alt="Dialog Box"></img>
+                                                    <p>{f.comments.length} comments</p>
+                                                </DialogBox>
+                                                <RepostBox>
+                                                    <img src={repostButton} alt="Repost Button"></img>
+                                                    <p>{f.repost_count} re-post</p>
+                                                </RepostBox>
+                                            </SideContainer>
+                                            <PostInfo>
+                                                <TopLine>
+                                                    <Username>{f.name}</Username>
+                                                </TopLine>
+                                                <PostDescription data-test="description" >
+                                                    {reactStringReplace(f.description, /#(\w+)/g, (match, i) => (
+                                                        <Link to={`/hashtag/${match}`} key={match + i} >#{match}</Link>
+                                                    ))}
+                                                </PostDescription>
+                                                <Metadata href={f.shared_link} target="_blank" data-test="link">
+                                                    <LinkInfo>
+                                                        <LinkTitle>{f.link_title}</LinkTitle>
+                                                        <LinkDescription>{f.link_description}</LinkDescription>
+                                                        <LinkURL>{f.shared_link}</LinkURL>
+                                                    </LinkInfo>
+                                                    <LinkImage src={f.link_image}></LinkImage>
+                                                </Metadata>
+                                            </PostInfo>
+                                        </PostContainer>
+                                        {Number(index) === Number(postIndex) && openComment &&
+                                            <ComentsBox>
+                                                {cm}
+                                            </ComentsBox>}
+                                    </BigContainer>)
+                            }
+
                             return (
                                 <BigContainer key={f.post_id} data-test="post">
                                     <PostContainer key={f.post_id} data-test="post">
@@ -337,10 +389,11 @@ export default function TimelinePage() {
                                                 <img src={dialogBox} alt="Dialog Box"></img>
                                                 <p>{f.comments.length} comments</p>
                                             </DialogBox>
-                                            <RepostBox onClick={() => handleRepost(f.post_id)}>
+                                            <RepostBox onClick={() => handleRepostModal(f.post_id)}>
                                                 <img src={repostButton} alt="Repost Button"></img>
                                                 <p>{f.repost_count} re-post</p>
                                             </RepostBox>
+                                            <RepostModal isOpen={openRepostModal} closeModal={() => setOpenRepostModal(!openRepostModal)} setOpenRepostModal post_id={f.post_id} token={userToken}></RepostModal>
                                         </SideContainer>
                                         <PostInfo>
                                             <TopLine>
@@ -519,6 +572,9 @@ const DialogBox = styled.div`
     justify-content: center;
     align-items: center;
     margin-top:15px;
+    img{
+        cursor:pointer;
+    }
 
     p{
         font-family: 'Lato';
@@ -535,6 +591,10 @@ const RepostBox = styled.div`
     justify-content: center;
     align-items: center;
     margin-top:20px;
+    
+    img{
+        cursor:pointer;
+    }
 
     p{
         font-family: 'Lato';
@@ -542,6 +602,8 @@ const RepostBox = styled.div`
         font-size: 11px;
         line-height: 13px;
         color: #FFFFFF;
+        margin-top:3px;
+
     }
 `
 
@@ -765,6 +827,11 @@ const Comments = styled.div`
     border-bottom: 1px solid #353535 ;
     padding: 15px 0;
     display: flex;
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
 `
 
 const ComentsBox = styled.div`
@@ -1002,6 +1069,29 @@ const ButtonBox = styled.div`
         background-color: transparent;
     }
 `
+
+const RepostBanner = styled.div`
+    display:flex;
+    justify-content: left;
+    align-items: flex-start;
+    padding: 10px 13px;
+    gap:7px;
+    width:100%;
+    height:66px;
+    background: #1E1E1E;
+    border-radius: 16px;
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 11px;
+    line-height: 13px;
+    margin-bottom:-33px;
+    a{
+        text-decoration: none;
+        color: #FFFFFF;
+    }
+`
+
 
 
 
